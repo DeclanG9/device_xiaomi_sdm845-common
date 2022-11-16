baka-hokage/xiaomi_sdm845

sdm845-common: parts: Add MiSound scenes
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: Ia0994eb82921448f7b45b677cc41b237bea142dc
Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -39,11 +39,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     private static final String PREF_HEADSET = "dirac_headset_pref";
     private static final String PREF_HIFI = "dirac_hifi_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
+    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
+    private ListPreference mScenes;
     private SwitchPreference mHifi;
     private DiracUtils mDiracUtils;
 
@@ -75,6 +77,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
         mPreset.setEnabled(!hifiEnable && enhancerEnabled);
         mHifi.setEnabled(mDiracUtils != null && enhancerEnabled);
+
+        mScenes = (ListPreference) findPreference(PREF_SCENE);
+        mScenes.setOnPreferenceChangeListener(this);
+        mScenes.setEnabled(mDiracUtils != null && enhancerEnabled);
     }
 
     @Override
@@ -94,6 +100,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
             case PREF_PRESET:
                mDiracUtils.setLevel(String.valueOf(newValue));
                 return true;
+            case PREF_SCENE:
+                mDiracUtils.setScenario(Integer.parseInt(newValue.toString()));
+                return true;
             default:
                 return false;
         }
@@ -108,6 +117,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mHifi.setEnabled(isChecked);
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
 
         if (!isChecked){
             mHifi.setChecked(false);

DeclanG9/device_xiaomi_sdm845-common

Change-Id: Ic3c90fa19fdc40c97d92d08fdbd8514247b37efc
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -36,11 +36,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
+    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
+    private ListPreference mScenes;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -60,6 +62,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
         mPreset.setEnabled(enhancerEnabled);
+
+        mScenes = (ListPreference) findPreference(PREF_SCENE);
+        mScenes.setOnPreferenceChangeListener(this);
+        mScenes.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -70,6 +76,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
                 return true;
             case PREF_PRESET:
                 DiracUtils.setLevel(String.valueOf(newValue));
+                return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
                 return true;
             default:
                 return false;
@@ -84,5 +93,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
     }
 }